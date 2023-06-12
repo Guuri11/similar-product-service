160b41beebdf8022fda31e91353113dd11dc464c
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-06-12 23:13:14 +0200

    first commit

---
+++ src/main/java/com/zara/similar/SimilarProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zara.similar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zara/similar/SimilarProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zara.similar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
